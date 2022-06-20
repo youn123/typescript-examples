61cebfdf7b14d9e343998f652bbe4855f7aee6c0
youn123 <younmin123@gmail.com>
2022-06-20 12:43:10 -0700

    update readme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,9 @@
 import type { NextPage } from 'next'
-import { createEditor, Descendant } from 'slate'
-import { Slate, Editable, withReact } from 'slate-react'
-import { useState } from 'react'
-
-const initialValue: Descendant[] = [
-  {
-    type: 'paragraph',
-    children: [{ text: 'A line of text in a paragraph.' }]
-  }
-]
 
 const Home: NextPage = () => {
-  const [editor] = useState(() => withReact(createEditor()))
-
   return (
     <div>
-      <Slate
-        editor={editor}
-        value={initialValue}
-      >
-        <Editable />
-      </Slate>
+      Home
     </div>
   )
 }
